b9cc45a78b1cd3bad3dac103dcd90795b40eb986
András Veres-Szentkirályi <vsza@vsza.hu>
2015-02-11 09:41:21 +0100

    [enh] logger: use raw mode

--- logger.erl
+++ logger.erl
@@ -18,10 +18,10 @@ log(Name, Source, Target) ->
     gen_server:cast(logger, {log, Name, Source, Target}).
 
 init(_Args) ->
-   file:open(?LOG_FILE, [append]).
+   file:open(?LOG_FILE, [append, raw]).
 
 handle_cast({log, Name, Source, Target}, Logfile) ->
    Msg = lists:flatten(io_lib:format("~s ~s from ~s dispatched to ~p~n", [iso_8601_fmt(erlang:localtime()), Name, inet_parse:ntoa(Source), Target])),
    io:format(Msg),
-   io:fwrite(Logfile, Msg, []),
+   file:write(Logfile, Msg),
    {noreply, Logfile}. 
