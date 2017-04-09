c1022c3bf5607e41fd59174906cc40a9f28c8a52
stf <s@ctrlc.hu>
2017-04-09 21:23:08 +0000

    [mod] lot's of small improvements, fighting bitrot

--- snifer.erl
+++ snifer.erl
@@ -1,16 +1,18 @@
 -module(snifer).
 -export([start/0, acceptor/1]).
 
+-include_lib("ssl/src/ssl_cipher.hrl").
+-include_lib("ssl/src/ssl_internal.hrl").
 -include_lib("ssl/src/tls_handshake.hrl").
 -include_lib("ssl/src/tls_record.hrl").
 
--define(MARK_FILE, "hosts.txt").
--define(MARK_PORT, 4000).
--define(LISTEN_PORT, 5000).
--define(DEFAULT_PORT, 6000).
+-define(MARK_DIR, "redirs/").
+-define(LISTEN_PORT, 443).
+-define(DEFAULT_PORT, 5000).
 
 start() ->
-	{ok, LSock} = gen_tcp:listen(?LISTEN_PORT, [binary, {active, true}, {packet, raw}]),
+	{ok, LSock} = gen_tcp:listen(?LISTEN_PORT, [binary,{reuseaddr, true}, {active, true}, {packet, raw}]),
+   logger:start(),
 	acceptor(LSock).
 
 acceptor(LSock) ->
@@ -18,9 +20,12 @@ acceptor(LSock) ->
 	gen_tcp:controlling_process(LSock, spawn(?MODULE, acceptor, [LSock])),
 	receive
 		{tcp, Sock, ClientHello} ->
-			Target = get_target(ClientHello),
+			{Name, Target} = get_target(ClientHello),
 			case gen_tcp:connect("localhost", Target, [binary, {active, true}, {packet, raw}]) of
-				{ok, TargetSock} -> gen_tcp:send(TargetSock, ClientHello), forwarder(Sock, TargetSock);
+				{ok, TargetSock} -> 
+               {ok, {Source, _ }} = inet:peername(Sock),
+               logger:log(Name, Source, Target),
+					gen_tcp:send(TargetSock, ClientHello), forwarder(Sock, TargetSock);
 				{error, Reason} -> io:format("Couldn't connect to target: ~p~n", [Reason])
 			end;
 		_ -> ok
@@ -42,24 +47,19 @@ forwarder(Socket1, Socket2) ->
 get_target(Packet) ->
 	try get_server_name(Packet) of
 		Name ->
-			{ok, File} = file:open(?MARK_FILE, [read, raw, read_ahead]),
-			case match_lines(File, Name) of
-				true -> ?MARK_PORT;
-				false -> ?DEFAULT_PORT
-			end
+         case file:open(?MARK_DIR ++ Name, [read, raw, read_ahead]) of
+            {ok, File} ->
+               case file:read_line(File) of
+                 {ok, Line} -> { Name, list_to_integer(string:strip(Line, right, $\n))}
+               end;
+            {error,enoent} -> io:format("Couldn't open file: ~p~n", [?MARK_DIR ++ Name]), {"file err", ?DEFAULT_PORT}
+         end
 	catch
-		E -> io:format("Couldn't extract SNI: ~p~n", [E]), ?DEFAULT_PORT
-	end.
-
-match_lines(File, Name) ->
-	case file:read_line(File) of
-		{ok, Line} ->
-			Name == string:strip(Line, right, $\n) orelse match_lines(File, Name);
-		eof -> file:close(File), false
+		E -> io:format("Couldn't extract SNI: ~p~n", [E]), {"no sni", ?DEFAULT_PORT}
 	end.
 
 get_server_name(Packet) ->
 	{[Record | _], _} = tls_record:get_tls_records(Packet, <<>>),
 	{[{ClientHello, _}], _} = tls_handshake:get_tls_handshake(
-		Record#ssl_tls.version, Record#ssl_tls.fragment, <<>>),
+		Record#ssl_tls.version, Record#ssl_tls.fragment, <<>>, #ssl_options{v2_hello_compatible=true} ),
 	ClientHello#client_hello.extensions#hello_extensions.sni#sni.hostname.
